fc1f4bb1e8d71033bf7d25f3508efe508b6a88da
FUNZHOU <130201517+Funzhou000@users.noreply.github.com>
2024-11-16 21:26:55 +0800

    Add files via upload

---
+++ 爱心（动态与静态）/爱心（动态与静态）/静态爱心.cpp
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//
+//int main() {
+//	for (float y = 2.0f; y > -2.0f; y -= 0.1f) {
+//		for (float x = -2.0f; x < 2.0f; x += 0.05f) {
+//			float a = x * x + y * y - 1;
+//			putchar(a * a * a - x * x * y * y * y < 0.0f ? 'v' : ' ');
+//		}
+//
+//		putchar('\n');
+//	}
+//}
